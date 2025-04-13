f8e2ee3959cd561a7e524b5b26b5a7cae68f575a
Alvaro <alvaro.rodriguez27@educa.madrid.org>
2025-04-14 00:42:39 +0200

    Primer commit front

---
+++ src/app/routes/index.mjs
@@ -0,0 +1,50 @@
+import express from "express"
+import path from "path"
+
+const router = express.Router()
+
+
+// Rutas para las vistas principales
+router.get("/", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/index.html"));
+});
+
+router.get("/index", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/index.html"));
+});
+
+router.get("/contact", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/contact.html"));
+});
+
+router.get("/events", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/events.html"));
+});
+
+router.get("/games", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/games.html"));
+});
+
+router.get("/register", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/register.html"));
+});
+
+router.get("/login", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/login.html"));
+});
+
+router.get("/play", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/play.html"));
+});
+
+router.get("/profile", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/profile.html"));
+});
+
+router.get("/addtokens", (req, res) => {
+    res.sendFile(path.join(path.resolve(), "views/addtokens.html"));
+});
+
+
+
+export default router
